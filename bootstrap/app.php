0468f0c2cd4bd3843cfd5b3f1055d10052f046f9
Najwan <muhdnajwan3@gmail.com>
2026-07-01 18:04:27 +0800

    Register web providers during bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,10 +2,20 @@
 
 use App\Http\Middleware\EnsureSupabaseAdmin;
 use App\Http\Middleware\EnsureSupabaseAuthenticated;
+use Illuminate\Cache\CacheServiceProvider;
+use Illuminate\Cookie\CookieServiceProvider;
+use Illuminate\Database\DatabaseServiceProvider;
+use Illuminate\Encryption\EncryptionServiceProvider;
+use Illuminate\Filesystem\FilesystemServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Queue\QueueServiceProvider;
+use Illuminate\Session\SessionServiceProvider;
+use Illuminate\Translation\TranslationServiceProvider;
+use Illuminate\Validation\ValidationServiceProvider;
+use Illuminate\View\ViewServiceProvider;
 
 $app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -24,6 +34,23 @@ $app = Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*'),
         );
     })->create();
+
+foreach ([
+    CacheServiceProvider::class,
+    CookieServiceProvider::class,
+    DatabaseServiceProvider::class,
+    EncryptionServiceProvider::class,
+    FilesystemServiceProvider::class,
+    QueueServiceProvider::class,
+    SessionServiceProvider::class,
+    TranslationServiceProvider::class,
+    ValidationServiceProvider::class,
+    ViewServiceProvider::class,
+] as $provider) {
+    if (! $app->providerIsLoaded($provider)) {
+        $app->register($provider);
+    }
+}
 
 if ($storagePath = getenv('APP_STORAGE_PATH')) {
     $app->useStoragePath($storagePath);
