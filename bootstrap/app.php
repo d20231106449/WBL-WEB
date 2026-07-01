f4110240d9c63d4c505abc79cbd9f4153f1eae7e
Najwan <muhdnajwan3@gmail.com>
2026-07-01 18:07:18 +0800

    Register providers through Laravel builder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,22 +2,24 @@
 
 use App\Http\Middleware\EnsureSupabaseAdmin;
 use App\Http\Middleware\EnsureSupabaseAuthenticated;
-use Illuminate\Cache\CacheServiceProvider;
-use Illuminate\Cookie\CookieServiceProvider;
-use Illuminate\Database\DatabaseServiceProvider;
-use Illuminate\Encryption\EncryptionServiceProvider;
-use Illuminate\Filesystem\FilesystemServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Queue\QueueServiceProvider;
-use Illuminate\Session\SessionServiceProvider;
-use Illuminate\Translation\TranslationServiceProvider;
-use Illuminate\Validation\ValidationServiceProvider;
-use Illuminate\View\ViewServiceProvider;
 
 $app = Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        Illuminate\Cache\CacheServiceProvider::class,
+        Illuminate\Cookie\CookieServiceProvider::class,
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\Queue\QueueServiceProvider::class,
+        Illuminate\Session\SessionServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class,
+        Illuminate\Validation\ValidationServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -34,23 +36,6 @@ $app = Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*'),
         );
     })->create();
-
-foreach ([
-    CacheServiceProvider::class,
-    CookieServiceProvider::class,
-    DatabaseServiceProvider::class,
-    EncryptionServiceProvider::class,
-    FilesystemServiceProvider::class,
-    QueueServiceProvider::class,
-    SessionServiceProvider::class,
-    TranslationServiceProvider::class,
-    ValidationServiceProvider::class,
-    ViewServiceProvider::class,
-] as $provider) {
-    if (! $app->providerIsLoaded($provider)) {
-        $app->register($provider);
-    }
-}
 
 if ($storagePath = getenv('APP_STORAGE_PATH')) {
     $app->useStoragePath($storagePath);
