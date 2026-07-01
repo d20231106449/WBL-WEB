31b82bfadeb29e7cbd431b7b6b1f7d8251318da4
Najwan <muhdnajwan3@gmail.com>
2026-07-01 17:57:32 +0800

    Register Laravel web providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,25 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use Illuminate\Cache\CacheServiceProvider;
+use Illuminate\Cookie\CookieServiceProvider;
+use Illuminate\Database\DatabaseServiceProvider;
+use Illuminate\Encryption\EncryptionServiceProvider;
+use Illuminate\Filesystem\FilesystemServiceProvider;
+use Illuminate\Queue\QueueServiceProvider;
+use Illuminate\Session\SessionServiceProvider;
+use Illuminate\Translation\TranslationServiceProvider;
+use Illuminate\View\ViewServiceProvider;
 
 return [
+    CacheServiceProvider::class,
+    CookieServiceProvider::class,
+    DatabaseServiceProvider::class,
+    EncryptionServiceProvider::class,
+    FilesystemServiceProvider::class,
+    QueueServiceProvider::class,
+    SessionServiceProvider::class,
+    TranslationServiceProvider::class,
+    ViewServiceProvider::class,
     AppServiceProvider::class,
 ];
